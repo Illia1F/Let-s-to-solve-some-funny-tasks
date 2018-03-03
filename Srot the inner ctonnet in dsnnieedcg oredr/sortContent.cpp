8ad94547a81942fc17041064baad67b50119762d
Illia <36344311+Illia1F@users.noreply.github.com>
2018-03-03 18:17:08 +0100

    Update sortContent.cpp

--- Srot the inner ctonnet in dsnnieedcg oredr/sortContent.cpp
+++ Srot the inner ctonnet in dsnnieedcg oredr/sortContent.cpp
@@ -12,7 +12,7 @@ std::string sortTheInnerContent(const char* words, const int length)
     int start = (result[0]==' ')?(2):(1);
 
     for(int i=start; i<length; i++){
-        // if we come across with the space or with the end of our string we will check next conditions
+        // if we come across with the whitespace or with the end of our string we will check next conditions
         if(result[i]==' ' || i == length-1){
             // set up the end value
             int end = (result[i]==' ')?(i-1):(i);\
