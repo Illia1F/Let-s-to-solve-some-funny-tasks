049e18511c9d664231b418831d4dcd9451acd2c7
Illia <36344311+Illia1F@users.noreply.github.com>
2018-03-08 17:19:49 +0100

    Update chessFun.cpp

--- ChessFun/chessFun.cpp
+++ ChessFun/chessFun.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-using namespace std;
 
 bool chessBoardCellColor(std::string cell1, std::string cell2) {
   return (cell1[0] + cell1[1] + cell2[0] + cell2[1]) % 2 == 0;
@@ -8,7 +7,7 @@ bool chessBoardCellColor(std::string cell1, std::string cell2) {
 
 int main()
 {
-    cout << chessBoardCellColor("A1", "C3") << endl;
+    std::cout << chessBoardCellColor("A1", "C3") << std::endl;
 
     return 0;
 }
